aa1cdfb1c1e25ec53558fa6d85d35b149df4d04d
Narmin Hajiyeva <s31656@pjwstk.edu.pl>
2026-03-31 02:04:55 +0200

    Add initial numbers array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Numbers loaded.");
+    }
+}
