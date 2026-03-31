717d489418324d98c43aba70e167f576dd1d4a56
Narmin Hajiyeva <s31656@pjwstk.edu.pl>
2026-03-31 02:31:18 +0200

    Add CalculateMin method

--- Program.cs
+++ Program.cs
@@ -1,63 +1,12 @@
-﻿using System;
-
-class Program
+﻿static int CalculateMin(int[] values)
 {
-    static void Main()
+    int min = values[0];
+
+    foreach (int v in values)
     {
-        int[] numbers = { 10, 20, 30, 40, 50 };
-
-        int sum = 0;
-
-        foreach (int n in numbers)
-        {
-            sum += n;
-        }
-
-        double average = CalculateAverage(numbers);
-        int min = FindMin(numbers);
-        int max = FindMax(numbers);
-
-        Console.WriteLine("Sum: " + sum);
-        Console.WriteLine("Average: " + average);
-        Console.WriteLine("Min: " + min);
-        Console.WriteLine("Max: " + max);
+        if (v < min)
+            min = v;
     }
 
-    static double CalculateAverage(int[] values)
-    {
-        int sum = 0;
-
-        foreach (int v in values)
-        {
-            sum += v;
-        }
-
-        return (double)sum / values.Length;
-    }
-
-    static int FindMin(int[] values)
-    {
-        int min = values[0];
-
-        foreach (int v in values)
-        {
-            if (v < min)
-                min = v;
-        }
-
-        return min;
-    }
-
-    static int FindMax(int[] values)
-    {
-        int max = values[0];
-
-        foreach (int v in values)
-        {
-            if (v > max)
-                max = v;
-        }
-
-        return max;
-    }
+    return min;
 }
