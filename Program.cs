c0b7819707db9ee44feeb16f27f556c2bd828deb
Narmin Hajiyeva <s31656@pjwstk.edu.pl>
2026-03-31 02:37:20 +0200

    Use CalculateMin in Main

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿static int CalculateMin(int[] values)
+﻿ static void Main()
 {
-    int min = values[0];
+    int[] numbers = { 10, 20, 30, 40, 50 };
 
-    foreach (int v in values)
+    int sum = 0;
+
+    foreach (int n in numbers)
     {
-        if (v < min)
-            min = v;
+        sum += n;
     }
 
-    return min;
+    double average = CalculateAverage(numbers);
+    int min = CalculateMin(numbers);
+    int max = FindMax(numbers);
+
+    Console.WriteLine("Sum: " + sum);
+    Console.WriteLine("Average: " + average);
+    Console.WriteLine("Min: " + min);
+    Console.WriteLine("Max: " + max);
 }
